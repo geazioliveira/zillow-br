fdb062cdb745fe9d60d46c7dd66058f93b573fad
Geazi Oliveira <geazioliveira@gmail.com>
2025-08-30 10:03:50 -0300

    Add support for shared workspaces and update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
-import nx from '@nx/eslint-plugin';
+import nx from '@nx/eslint-plugin'
+import pluginImport from 'eslint-plugin-import'
 
 export default [
   ...nx.configs['flat/base'],
@@ -9,6 +10,9 @@ export default [
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    plugins: {
+      import: pluginImport,
+    },
     rules: {
       '@nx/enforce-module-boundaries': [
         'error',
@@ -23,6 +27,31 @@ export default [
           ],
         },
       ],
+      // Enforce import order
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // Node.js builtins
+            'external', // npm libraries
+            'internal', // Your aliases (e.g. @app/*)
+            ['parent', 'sibling', 'index'], // Relative imports
+          ],
+          pathGroups: [
+            {
+              pattern: '@/mui', // <-- adjust if you use tsconfig paths
+              group: 'external',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc', // ascending
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always', // enforce blank line between groups
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
   {
@@ -39,4 +68,4 @@ export default [
     // Override or add rules here
     rules: {},
   },
-];
+]
