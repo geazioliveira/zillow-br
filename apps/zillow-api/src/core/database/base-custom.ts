b5e13d443541f8d41b607c2709137e18877f85e8
Geazi Oliveira <geazioliveira@gmail.com>
2025-09-01 19:12:42 -0300

    Add `status` column to `BaseCustom` entity and define `Status` enum
    - Introduced `status` column with `enum` type to `BaseCustom` entity for enhanced state management.
    - Added `Status` enum with values: `ACTIVE`, `INACTIVE`, `PENDING`, and `DELETED`.

--- apps/zillow-api/src/core/database/base-custom.ts
+++ apps/zillow-api/src/core/database/base-custom.ts
@@ -1,14 +1,23 @@
 import {
   BaseEntity,
+  Column,
   CreateDateColumn,
   DeleteDateColumn,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm'
+import { Status } from './enums/status'
 
 export class BaseCustom extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @Column({
+    type: 'enum',
+    enum: Status,
+    default: Status.ACTIVE,
+  })
+  status: Status
 
   @CreateDateColumn()
   createdAt: Date
